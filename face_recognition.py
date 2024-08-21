b1696577873e34a48d888592b1bd5980fdc051ba
Abhijeet Sasmal <abhijeetsasmal33@gmail.com>
2024-08-21 18:23:57 +0530

    made more efficient

--- face_recognition.py
+++ face_recognition.py
@@ -13,7 +13,9 @@ def image_to_polygons(image_path):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-    return thresh
+    img[np.where(thresh, False, True)] = 0
+
+    return img
 
 
 face_cascade = cv2.CascadeClassifier("harcascade/haarcascade_frontalface_default.xml")
@@ -24,43 +26,51 @@ glasses_clr = cv2.imread("glasses.png", cv2.IMREAD_UNCHANGED)
 
 
 cap = cv2.VideoCapture(0)
-
+process = True
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    back = frame
+    if process:
 
-    for x, y, w, h in faces:
-        roi_gray = gray[y : y + h, x : x + w]
-        roi_color = frame[y : y + h, x : x + w]
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        back = frame
 
-        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for x, y, w, h in faces:
+            roi_gray = gray[y : y + h, x : x + w]
+            roi_color = frame[y : y + h, x : x + w]
 
-        eyes_frame = frame
-        eyes_frame = create_rectangle(
-            img=eyes_frame, x=x, y=y, x1=x + w, y1=y + h, color=(255, 0, 0)
-        )
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            if len(eyes) != 2:
+                continue
 
-        try:
-            ex, ey, ew, eh = eyes[0]
-            ex1, ey1, ew1, eh1 = eyes[1]
+            eyes_frame = frame
+            eyes_frame = create_rectangle(
+                img=eyes_frame, x=x, y=y, x1=x + w, y1=y + h, color=(255, 0, 0)
+            )
 
-            glasses = image_to_polygons("glasses.png")
+            try:
+                ex, ey, ew, eh = eyes[0]
+                ex1, ey1, ew1, eh1 = eyes[1]
 
-            roi = eyes_frame[ey + y : ey1 + ew1 + y, ex + x - 10 : ex1 + eh1 + x + 10]
-            y, x, z = roi.shape
-            resized_glassses = cv2.resize(glasses, (x, y))
+                glasses = image_to_polygons("glasses.png")
+                cv2.imshow("glass", glasses)
+                roi = eyes_frame[
+                    ey + y : ey1 + ew1 + y, ex + x - 10 : ex1 + eh1 + x + 10
+                ]
+                y, x, z = roi.shape
+                resized_glassses = cv2.resize(glasses, (x, y))
 
-            roi[np.where(resized_glassses, False, True)] = 0
+                roi[np.where(resized_glassses, False, True)] = 0
 
-        except Exception as e:
-            pass
+            except Exception as e:
+                print(e)
+                pass
 
         cv2.imshow("frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    process = not process
 
 cap.release()
 cv2.destroyAllWindows()
